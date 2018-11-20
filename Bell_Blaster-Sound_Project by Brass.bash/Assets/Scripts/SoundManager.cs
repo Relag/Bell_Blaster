148d09b0be0f9ea80c738e4c010d92253c59d296
Relag93 <rejean.lagasse93@gmail.com>
2018-11-20 16:33:59 -0500

    Created a basic Sound Manager

---
+++ Bell_Blaster-Sound_Project by Brass.bash/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public AudioClip[] bellSounds;
+
+    public AudioClip[] BGM;
+
+    public static SoundManager Instance;
+	// Use this for initialization
+	void Awake ()
+    {
+	    if(Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad (gameObject);
+	}
+
+    public void RingBell(int x, AudioSource playFrom)
+    {
+        playFrom.loop = false;
+        playFrom.PlayOneShot(bellSounds[x]);
+    }
+
+    public void PlayMusic(int x, AudioSource playFrom)
+    {
+        playFrom.loop = true;
+        playFrom.clip = BGM[x];
+        playFrom.Play();
+    }
+
+}
