62a1f7eb2ee7a6594f96f1aa28def37bbc47e176
Relag <44652125+Relag@users.noreply.github.com>
2018-11-21 15:08:09 -0500

    Made BGM follow player, Created the SoundManager Prefab

---
+++ Bell_Blaster-Sound_Project by Brass.bash/Assets/Scripts/BackGroundMusic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundMusic : MonoBehaviour {
+
+    public AudioSource BGM;
+
+	// Use this for initialization
+	void Start ()
+    {
+        BGM = GetComponent<AudioSource>();
+        SoundManager.Instance.PlayMusic(0, BGM);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
