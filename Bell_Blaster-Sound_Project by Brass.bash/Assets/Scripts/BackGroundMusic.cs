ad4a3ddec8fdcc9ed32ddd07166f24677219c10f
Relag <44652125+Relag@users.noreply.github.com>
2018-11-28 14:55:52 -0500

    Made the int for song track number public, added comments on how to implement.

--- Bell_Blaster-Sound_Project by Brass.bash/Assets/Scripts/BackGroundMusic.cs
+++ Bell_Blaster-Sound_Project by Brass.bash/Assets/Scripts/BackGroundMusic.cs
@@ -2,19 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/* NB: Attach this script to the player. Ensure that the player, or child-object has an audiosource. The public int
+  corresponds to the array of music found in SoundManager.cs.  */ 
 public class BackGroundMusic : MonoBehaviour {
 
-    public AudioSource BGM;
+    private AudioSource BGM;
+    public int songNumber;
 
 	// Use this for initialization
 	void Start ()
     {
         BGM = GetComponent<AudioSource>();
-        SoundManager.Instance.PlayMusic(0, BGM);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+        SoundManager.Instance.PlayMusic(songNumber, BGM);
 	}
 }
