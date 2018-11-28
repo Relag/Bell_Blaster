b5854ea1b5574d3f045016b1b21e8ca332e33178
chuckiehabs <cpearson_99_ca@hotmail.com>
2018-11-28 16:45:49 -0500

    -Added door animations -added prefabs for music notes -added materials for music notes -fixed a script error for Charles background music

---
+++ Bell_Blaster-Sound_Project by Brass.bash/Assets/Scripts/CharlesBGM.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharlesBGM : MonoBehaviour {
+
+    public AudioSource BGMC;
+
+	void Start ()
+    {
+        BGMC = GetComponent<AudioSource>();
+        SoundManager.Instance.PlayMusic(0, BGMC);
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
