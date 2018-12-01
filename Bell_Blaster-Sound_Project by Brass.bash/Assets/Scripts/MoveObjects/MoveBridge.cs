690054dc7ab0397a72f27a20556ba44300a6bf07
Relag <44652125+Relag@users.noreply.github.com>
2018-12-01 15:33:54 -0500

    Created folder for puzzle movement scripts

---
+++ Bell_Blaster-Sound_Project by Brass.bash/Assets/Scripts/MoveObjects/MoveBridge.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBridge : MonoBehaviour
+{
+
+    private Vector3 normal;
+    // Use this for initialization
+    void Start()
+    {
+        normal = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.Instance.currentNote == GameManager.BellNotes.F4)
+        {
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+            transform.position = new Vector3(normal.x + 15, normal.y, normal.z + 22);
+        }
+        else
+        {
+            transform.position = normal;
+        }
+    }
+}
---
+++ Bell_Blaster-Sound_Project by Brass.bash/Assets/Scripts/MoveObjects/MoveTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTest : MonoBehaviour {
+
+    private Vector3 normal;
+	// Use this for initialization
+	void Start () {
+        normal = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (GameManager.Instance.currentNote == GameManager.BellNotes.D2)
+        {
+            transform.position = new Vector3(normal.x + 5, normal.y, normal.z);
+        }
+        else
+        {
+            transform.position = normal;
+        }
+	}
+}
