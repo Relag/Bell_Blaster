145fb8156d3db996141505ee803d62daa99148f7
Relag <44652125+Relag@users.noreply.github.com>
2018-12-01 13:31:56 -0500

    created GameManager, Enum corrosponds to sides of bell

---
+++ Bell_Blaster-Sound_Project by Brass.bash/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public enum  BellNotes { C1, D2, E3, F4, G5, A6, B7, C8}
+    public BellNotes currentNote;
+    public static GameManager Instance;
+
+	// Use this for initialization
+	void Awake () {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
