bde49c986a4e1817aec7e232b641a5ccdf1f2994
Nicholas Shortt <Nicholas.Shortt@hotmail.com>
2018-12-12 15:36:11 -0500

    Nick Level
    Had to re add my level.

---
+++ Bell_Blaster-Sound_Project by Brass.bash/Assets/Nick's Assets/Script/PlatformScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformScript : MonoBehaviour {
+
+    public GameObject[] platforms;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        for (int i = 0; i <= 7; i++)
+        {
+            if(platforms[i].tag == GameManager.Instance.currentNote.ToString())
+            {
+                platforms[i].SetActive(true);
+            }
+            else
+            {
+                platforms[i].SetActive(false);
+            }
+        }
+	}
+}
