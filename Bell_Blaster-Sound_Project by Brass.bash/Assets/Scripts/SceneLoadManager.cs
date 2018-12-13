fec670cd940a9dbe6b5c0654915a468bee39c54e
Yash <farceur.kaush@gmail.com>
2018-12-12 20:47:14 -0500

    pushing a bunch of stuff with a load Manager script

---
+++ Bell_Blaster-Sound_Project by Brass.bash/Assets/Scripts/SceneLoadManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadManager : MonoBehaviour {
+
+    private int levelIndex;         //stores the level index
+
+    public void FixedUpdate()
+    {
+        // fetch the active scene index and store it in levelIndex
+        levelIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("levelCube1"))
+        {
+            Debug.Log("triggerZone trigger enter");
+            SceneManager.LoadScene(1);
+        }
+        else if (collider.CompareTag("levelCube2"))
+        {
+            Debug.Log("triggerZone trigger enter");
+            SceneManager.LoadScene(2);
+        }
+        else if (collider.CompareTag("levelCube3"))
+        {
+            Debug.Log("triggerZone trigger enter");
+            SceneManager.LoadScene(3);
+        }
+    }
+
+
+
+}
