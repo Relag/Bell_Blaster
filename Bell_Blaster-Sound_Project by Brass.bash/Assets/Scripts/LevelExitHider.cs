c687d55b63cd1c9824e81cdcaded8acc84d0969a
Nicholas Shortt <Nicholas.Shortt@hotmail.com>
2018-12-12 20:13:04 -0500

    Made a script to hide an object if you have a score of 8 or more

---
+++ Bell_Blaster-Sound_Project by Brass.bash/Assets/Scripts/LevelExitHider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelExitHider : MonoBehaviour {
+
+    public GameObject sceneExit;
+
+    private void Update()
+    {
+        if(GameManager.Instance.score >= 8)
+        {
+            sceneExit.SetActive(true);
+        }
+        else
+        {
+            sceneExit.SetActive(false);
+        }
+    }
+}
